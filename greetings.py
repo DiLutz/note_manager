7115dfead5d5324f66f65b3256f8d422fcd39b74
Dmitriy Goncharov <goncahrovdi98@gmail.com>
2025-01-11 16:35:38 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,16 @@
+
+
+
+
+username = "Dmitriy Goncharov"
+title = "Grade 1"
+content = "Homework 1"
+status = "Active"
+created_date = "09.01.2025"
+issue_date = "11.01.2025"
+
+print("Dmitriy Goncharov", username)
+print("Grade 1", title)
+print("Active", content)
+print("09.01.2025", created_date)
+print("11.01.2025", issue_date)
