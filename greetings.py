43756c413c5a1039f8de33bf6a483bba0def38db
Dmitriy Goncharov <goncahrovdi98@gmail.com>
2025-01-11 17:30:28 +0300

    greetings.py

--- greetings.py
+++ greetings.py
@@ -9,8 +9,9 @@ status = "Active"
 created_date = "09.01.2025"
 issue_date = "11.01.2025"
 
-print("Dmitriy Goncharov", username)
-print("Grade 1", title)
-print("Active", content)
-print("09.01.2025", created_date)
-print("11.01.2025", issue_date)
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", created_date)
+print("Дата создания заметки:", issue_date)
+print("Дата истечения заметки:", issue_date)
