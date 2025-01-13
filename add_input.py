19ae8d783e26ed8c139d2722d5a3d76415852b1c
Dmitriy Goncharov <goncahrovdi98@gmail.com>
2025-01-13 21:09:44 +0300

    Update add_input.py

--- add_input.py
+++ add_input.py
@@ -1,15 +1,20 @@
 
 username = input("Введите имя пользователя: ")
+title = input("Введите заголовок заметки: ")
 content = input("Введите описание заметки: ")
+status = input("Введите статус заметки: ")
 created_date = input("Введите дату создания заметки в формате 'день.месяц.год': ")
+issue_date = input("Введите дату истечения заметки в формате 'день.месяц.год': ")
 
 title1 = input("Введите первый заголовок: ")
 title2 = input("Введите второй заголовок: ")
 title3 = input("Введите третий заголовок: ")
-titles = title1, title2, title3
+titles = [title1, title2, title3]
 
-print("\nВы ввели следующие данные:")
+
 print("Имя пользователя:", username)
 print("Заголвоки заметки:", titles)
 print("Описание заметки:", content)
+print("Статус заметки:", status)
 print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
