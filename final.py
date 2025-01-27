c7aebc9467320044fd00db47193f6f89a376ff3f
Dmitriy Goncharov <goncahrovdi98@gmail.com>
2025-01-27 16:39:03 +0300

    final.py

--- final.py
+++ final.py
@@ -1,16 +1,18 @@
+note = []
 
-note = {}
+username = input("Введите имя пользователя: ")
+content = input("Введите описание заметки: ")
+status = input("Введите статус заметки: ")
+created_date = input("Введите дату создания заметки в формате 'день.месяц.год': ")
+issue_date = input("Введите дату истечения заметки в формате 'день.месяц.год': ")
+data = [username, content, status, created_date, issue_date]
+note.append(data)
 
-note["username"] = input("Введите имя пользователя: ")
-note["content"] = input("Введите описание заметки: ")
-note["status"] = input("Введите статус заметки: ")
-note["created_date"] = input("Введите дату создания заметки в формате 'день.месяц.год': ")
-note["issue_date"] = input("Введите дату истечения заметки в формате 'день.месяц.год': ")
-
-title1 = input("Введите первый заголовок заметки: ")
-title2 = input("Введите второй заголовок заметки: ")
-title3 = input("Введите третий заголовок заметки: ")
-note["titles"] = [title1, title2, title3]
+title1 = input("Введите первый заголовок: ")
+title2 = input("Введите второй заголовок: ")
+title3 = input("Введите третий заголовок: ")
+titles = [title1, title2, title3]
+note.append(titles)
 
 print(note)
 
